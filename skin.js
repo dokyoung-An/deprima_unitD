a0b49a1de35eabbe4d865e0838940b3cf5d8242b
Dokyung An <tea-co@naver.com>
2024-01-31 11:24:18 +0900

    주소

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.0.8/20017
 // Filename: ???? ??D.ggsk
-// Generated 2024-01-31T10:31:24
+// Generated 2024-01-31T11:23:16
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_roomchoice', 2, false, { ignoreInState: 0  });
@@ -13459,7 +13459,7 @@ function pano2vrSkin(player,base) {
 		}
 		me._c_type.logicBlock_backgroundcolor();
 		me._c_type.onclick=function (e) {
-			player.openUrl("https:\/\/funny-croissant-03ab5e.netlify.app","_self");
+			player.openUrl("https:\/\/effortless-tiramisu-1c2b58.netlify.app\/","_self");
 		}
 		me._c_type.onmouseover=function (e) {
 			me.elementMouseOver['c_type']=true;
